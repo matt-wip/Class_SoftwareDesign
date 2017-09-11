81b14e5ab1c525344fa933f2d516d24153bf6774
Matthew Wipfler <matthew-wipfler@uiowa.edu>
2017-09-11 12:10:21 -0500

    Completed S1_Computus_Hard. Will make slight alterations in future.

---
+++ lab/20170911-Quiz/src/Date.java
@@ -0,0 +1,93 @@
+// Fig. 8.7: Date.java 
+// Date class declaration.
+
+public class Date {
+    private static final int[] daysPerMonth =
+            {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    private int month; // 1-12
+    private int day; // 1-31 based on month
+    private int year; // any year
+
+    public int getMonth() {
+        return month;
+    }
+
+    public void setMonth(int month) {
+        this.month = month;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public void setDay(int day) {
+        this.day = day;
+    }
+
+    private static void CheckDayOfMonth(int day, int month) {
+        if (day <= 0 ||
+                (day > daysPerMonth[month] && !(month == 2 && day == 29)))
+            throw new IllegalArgumentException("day (" + day +
+                    ") out-of-range for the specified month and year");
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public void setDate(int day, int month, int year){
+        CheckDayOfMonth(day, month);
+        this.day = day;
+        this.month = month;
+        this.year = year;
+    }
+
+    // constructor: confirm proper value for month and day given the year
+    public Date(int month, int day, int year) {
+        // check if month in range
+        if (month <= 0 || month > 12)
+            throw new IllegalArgumentException(
+                    "month (" + month + ") must be 1-12");
+
+        // check if day in range for month
+        CheckDayOfMonth(day, month);
+
+        // check for leap year if month is 2 and day is 29
+        if (month == 2 && day == 29 && !(year % 400 == 0 ||
+                (year % 4 == 0 && year % 100 != 0)))
+            throw new IllegalArgumentException("day (" + day +
+                    ") out-of-range for the specified month and year");
+
+        this.month = month;
+        this.day = day;
+        this.year = year;
+
+        System.out.printf(
+                "Date object constructor for date %s%n", this);
+    }
+
+    // return a String of the form month/day/year
+    public String toString() {
+        return String.format("%d/%d/%d", month, day, year);
+    }
+} // end class Date
+
+
+/**************************************************************************
+ * (C) Copyright 1992-2014 by Deitel & Associates, Inc. and               *
+ * Pearson Education, Inc. All Rights Reserved.                           *
+ *                                                                        *
+ * DISCLAIMER: The authors and publisher of this book have used their     *
+ * best efforts in preparing the book. These efforts include the          *
+ * development, research, and testing of the theories and programs        *
+ * to determine their effectiveness. The authors and publisher make       *
+ * no warranty of any kind, expressed or implied, with regard to these    *
+ * programs or to the documentation contained in these books. The authors *
+ * and publisher shall not be liable in any event for incidental or       *
+ * consequential damages in connection with, or arising out of, the       *
+ * furnishing, performance, or use of these programs.                     *
+ *************************************************************************/
