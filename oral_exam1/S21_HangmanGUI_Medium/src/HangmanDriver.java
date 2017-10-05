095ad1aab140aa632931fba12301d8dc6e21468a
Matthew Wipfler <matthew-wipfler@uiowa.edu>
2017-10-05 11:27:33 -0500

    Initial Hangman layout

---
+++ oral_exam1/S21_HangmanGUI_Medium/src/HangmanDriver.java
@@ -0,0 +1,12 @@
+/*
+ * Name: Matthew Wipfler
+ * Date: 10/5/17
+ * File: HangmanDriver.java
+ */
+
+public class HangmanDriver {
+
+    public static void main(String[] args){
+        HangmanWindow kMainWindow = new HangmanWindow(10);
+    }
+}
