c089cabb08672dc4feeb04595e746e276c08e6a8
Matthew Wipfler <matthew-wipfler@uiowa.edu>
2017-10-05 14:28:01 -0500

    Rewrote HangmanMedium to use MVC layout Finished behavior of code

--- oral_exam1/S21_HangmanGUI_Medium/src/HangmanDriver.java
+++ oral_exam1/S21_HangmanGUI_Medium/src/HangmanDriver.java
@@ -4,9 +4,16 @@
  * File: HangmanDriver.java
  */
 
+import com.sun.xml.internal.ws.policy.subject.WsdlBindingSubject;
+
+import javax.swing.*;
+
 public class HangmanDriver {
 
     public static void main(String[] args){
-        HangmanWindow kMainWindow = new HangmanWindow(10);
+        HangmanModel hangmanModel = new HangmanModel(10, JOptionPane.showInputDialog(
+               null,"Player 1, type in your secret word/phrase","Input word/phrase", JOptionPane.QUESTION_MESSAGE));
+        HangmanView hangmanView = new HangmanView();
+        HangmanController hangmanController = new HangmanController(hangmanView, hangmanModel);
     }
 }
