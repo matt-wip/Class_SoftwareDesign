474ff007b930808ef53a6fee79010bf4eed82024
Matthew Wipfler <matthew-wipfler@uiowa.edu>
2017-11-06 12:15:18 -0600

    Quiz for 11/06/17. TipCalculator

--- lab/20171106_Quiz/src/TipController.java
+++ lab/20171106_Quiz/src/TipController.java
@@ -1,8 +1,12 @@
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
 import javafx.scene.control.TextField;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.text.NumberFormat;
 
 public class TipController {
 
@@ -24,6 +28,14 @@ public class TipController {
     @FXML
     private Button calculateButton;
 
+    @FXML
+    private void calculateButtonPressed(ActionEvent event){
+        try{
 
 
+        }
+        catch(Exception e) {
+
+        }
+    }
 }
