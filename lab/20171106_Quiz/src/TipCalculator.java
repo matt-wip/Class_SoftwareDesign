c543b3d6bf0397afc76dffd321428a41bd2fecf4
Matthew Wipfler <matthew-wipfler@uiowa.edu>
2017-11-06 12:07:30 -0600

    Quiz for 11/06/17. TipCalculator

---
+++ lab/20171106_Quiz/src/TipCalculator.java
@@ -0,0 +1,27 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class TipCalculator extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        try{
+            Parent root = FXMLLoader.load(getClass().getResource("TipCalculator.fxml"));
+            Scene mainScene = new Scene(root);
+
+            primaryStage.setTitle("Tip Calculator");
+            primaryStage.setScene(mainScene);
+            primaryStage.show();
+        }
+        catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
+}
---
+++ lab/20171106_Quiz/src/TipController.java
@@ -0,0 +1,29 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
+import javafx.scene.control.TextField;
+
+public class TipController {
+
+    @FXML
+    private TextField amountTextField;
+
+    @FXML
+    private TextField tipPercentageSlider;
+
+    @FXML
+    private TextField totalTextField;
+
+    @FXML
+    private Slider tipTextField;
+
+    @FXML
+    private Label tipPercentageLabel;
+
+    @FXML
+    private Button calculateButton;
+
+
+
+}
