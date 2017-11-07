0495190f64fcda01941fe3def5225b75ddf60b83
Matthew Wipfler <matthew-wipfler@uiowa.edu>
2017-11-06 19:14:07 -0600

    Quiz for 11/06/17. TipCalculator

--- lab/20171106_Quiz/src/TipController.java
+++ lab/20171106_Quiz/src/TipController.java
@@ -1,26 +1,35 @@
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
 import javafx.scene.control.TextField;
+import jdk.nashorn.internal.runtime.ECMAException;
+
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.text.NumberFormat;
 
 public class TipController {
+    // From TipCalculatorController.java in class_demos-> wk25
+    // formatters for currency and percentages
+    private static final NumberFormat currency = NumberFormat.getCurrencyInstance();
+    private static final NumberFormat percent = NumberFormat.getPercentInstance();
+    private BigDecimal tipPercentage = new BigDecimal(0.15); // 15% default
 
     @FXML
     private TextField amountTextField;
 
     @FXML
-    private TextField tipPercentageSlider;
+    private TextField tipValue;
 
     @FXML
     private TextField totalTextField;
 
     @FXML
-    private Slider tipTextField;
+    private Slider tipSlider;
 
     @FXML
     private Label tipPercentageLabel;
@@ -30,12 +39,35 @@ public class TipController {
 
     @FXML
     private void calculateButtonPressed(ActionEvent event){
-        try{
+        try {
+            BigDecimal amount = new BigDecimal(amountTextField.getText());
+            BigDecimal tip = amount.multiply(tipPercentage);
+            BigDecimal total = amount.add(tip);
 
-
+            tipValue.setText(currency.format(tip));
+            totalTextField.setText(currency.format(total));
+        } catch (NumberFormatException ex) {
+            amountTextField.setText("Enter amount");
+            amountTextField.selectAll();
+            amountTextField.requestFocus();
         }
-        catch(Exception e) {
+    }
 
-        }
+    public void initialize(){
+        // 0-4 rounds down, 5-9 rounds up
+        currency.setRoundingMode(RoundingMode.HALF_UP);
+
+        // listener for changes to tipPercentageSlider's value
+        tipSlider.valueProperty().addListener(
+                new ChangeListener<Number>() {
+                    @Override
+                    public void changed(ObservableValue<? extends Number> ov,
+                                        Number oldValue, Number newValue) {
+                        tipPercentage = BigDecimal.valueOf(newValue.intValue() / 100.0);
+                        tipPercentageLabel.setText(percent.format(tipPercentage));
+                        calculateButtonPressed(null);
+                    }
+                }
+        );
     }
 }
